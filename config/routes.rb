96bbb85f1868a3d46e944c9d9fd895ee98467514
Shwheelz <sfbesong@gmail.com>
2015-03-20 18:23:14 -0400

    added votes to topics view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'topics#index'
+
+    resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
